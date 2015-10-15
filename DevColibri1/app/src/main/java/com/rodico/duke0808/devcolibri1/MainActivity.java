63fa19cb60fba6e7da1c289b1083f71babac467d
roman-trokachevskyi <dukenukem3d0808@gmail.com>
2015-10-15 12:00:06 +0300

    a

---
+++ DevColibri1/app/src/main/java/com/rodico/duke0808/devcolibri1/MainActivity.java
@@ -0,0 +1,13 @@
+package com.rodico.duke0808.devcolibri1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
