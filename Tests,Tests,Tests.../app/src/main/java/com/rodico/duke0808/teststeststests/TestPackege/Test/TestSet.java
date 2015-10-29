0a42027b0abb9064ca6dd36dcea99ea079db8539
roman-trokachevskyi <dukenukem3d0808@gmail.com>
2015-10-29 10:52:09 +0200

    Added few tests. fixed question fragm layout

--- Tests,Tests,Tests.../app/src/main/java/com/rodico/duke0808/teststeststests/TestPackege/Test/TestSet.java
+++ Tests,Tests,Tests.../app/src/main/java/com/rodico/duke0808/teststeststests/TestPackege/Test/TestSet.java
@@ -54,6 +54,8 @@ public class TestSet {
         tests.add(getTestOverWeight());
         tests.add(getTestSelfConfident());
         tests.add(getEfficientTest());
+        tests.add(getTestArtistics());
+        tests.add(getTestBusiness());
 
         return tests;
     }
@@ -277,7 +279,6 @@ public class TestSet {
         return test;
 
     }
-
     public static Test getTestTimeUsing(){
         ArrayList<TestQuestion> questions = new ArrayList<>();
 
@@ -561,6 +562,225 @@ public class TestSet {
                     return "«Точка отсчета». Об этих людях говорят вообще шопотом. Они живут своей жизнью. Жизнью Победителей. Они путешествуют сколько хотят, ездят на лучших автомобилях и наслаждаются всеми благами этого мира. Обычно они владельцы компаний или Топ-менеджеры потому, что знают цену времени. Они пример для всех!";
                 }
                 return "EROR!!!";
+            }
+        };
+        return test;
+    }
+    public static Test getTestArtistics(){
+        ArrayList<TestQuestion> questions = new ArrayList<>();
+
+        String[] answers1;
+        answers1 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("1. Есть ли разница между словами тон и нюанс? "
+                ,2,answers1,1));
+        String[] answers2;
+        answers2 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("2. Можете ли вы жить в неуютной, скучной квартире и не замечать этого?"
+                ,2,answers2,2));
+        String[] answers3;
+        answers3 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("3. Вы любите рисовать?"
+                ,2,answers3,1));
+        String[] answers4;
+        answers4 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("4. Одеваетесь ли вы, опираясь не на моду, а на собственный вкус?"
+                ,2,answers4,1));
+        String[] answers5;
+        answers5 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("5. Говорят ли вам что-нибудь следующие имена: Мане, Ван Дейк, Хосе Рибера, Фальконе?"
+                ,2,answers5,1));
+        String[] answers6;
+        answers6 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("6. У вас очень плохой почерк?"
+                ,2,answers6,2));
+        String[] answers7;
+        answers7 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("7. Стараетесь ли вы одеваться в одной цветовой гамме?"
+                ,2,answers7,1));
+        String[] answers8;
+        answers8 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("8. Любите ли вы посещать музеи?"
+                ,2,answers8,1));
+        String[] answers9;
+        answers9 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("9. Остановитесь ли по пути, чтобы полюбоваться закатом?"
+                ,2,answers9,2));
+        String[] answers10;
+        answers10 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("10. Вы любите чертить геометрические фигуры? "
+                ,2,answers10,2));
+        String[] answers11;
+        answers11 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("11. Часто ли посещаете художественные салоны?"
+                ,2,answers11,1));
+        String[] answers12;
+        answers12 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("12. Склонны ли вы долго бродить по улицам?"
+                ,2,answers12,1));
+        String[] answers13;
+        answers13 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("13. Любите ли одиночество?"
+                ,2,answers13,1));
+        String[] answers14;
+        answers14 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("14. Кажется ли вам абсурдом, когда кто-то начинает декламировать стихи?"
+                ,2,answers14,2));
+        String[] answers15;
+        answers15 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("15. Слушаете ли вы музыку только ради развлечения?"
+                ,2,answers15,2));
+        String[] answers16;
+        answers16 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("16. Надолго ли запоминаете красивые пейзажи?"
+                ,2,answers16,1));
+        String[] answers17;
+        answers17 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("17. Кажутся ли вам очень красивыми морские камни?"
+                ,2,answers17,1));
+        String[] answers18;
+        answers18 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("18. Любите ли новые встречи и знакомства?"
+                ,2,answers18,1));
+        String[] answers19;
+        answers19 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("19. Нравится ли вам читать стихи вслух?"
+                ,2,answers19,1));
+        String[] answers20;
+        answers20 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("20. Не было ли желания разрисовать картинами стены своей комнаты?"
+                ,2,answers20,1));
+        String[] answers21;
+        answers21 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("21. Часто ли меняете прическу?"
+                ,2,answers21,1));
+        String[] answers22;
+        answers22 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("22. Часто ли переставляете мебель у себя дома?"
+                ,2,answers22,1));
+        String[] answers23;
+        answers23 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("23. Вы пробовали когда-нибудь сочинять песенки?"
+                ,2,answers23,1));
+        String[] answers24;
+        answers24 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("24. Пишете ли вы стихи?"
+                ,2,answers24,1));
+        Test test  = new Test("Артистические наклонности","Некоторые любят петь. Другие могут целыми днями слушать музыку. Такие люди обычно тонко чувствуют все, что происходит вокруг. Проще говоря, они любят красоту. Для них искусство — украшение жизни. А есть люди, для которых область искусства — что-то совершенно ненужное и бесполезное. Ответив на предлагаемые вопросы, вы узнаете к какой группе Вы относитесь. ", questions){
+            @Override
+            public String getResult() {
+                if (this.getCounterOfRight()>16){
+                    return "Можно сказать совершенно определенно одно:  у вас есть чувство красоты, свойственное натуре артистической.";
+                }
+                if (this.getCounterOfRight()>=8&&this.getCounterOfRight()<=16){
+                    return "Что ж, вы иногда витаете в облаках, но, хотя красота вам и небезразлична, вы скорее рационалист. Короче говоря, вы всегда сочетаете приятное с полезным.";
+                }
+                if (this.getCounterOfRight()>=0&&this.getCounterOfRight()<=4){
+                    return "Такого человека, как вы, вряд ли остановит красота восхода или заката.";
+                }
+
+                return "EROR";
+            }
+        };
+        return test;
+    }
+    public static Test getTestBusiness(){
+        ArrayList<TestQuestion> questions = new ArrayList<>();
+
+        String[] answers1;
+        answers1 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("1. Умеете ли вы доводить начатое дело до конца, несмотря на возникающие препятствия?"
+                ,2,answers1,1));
+        String[] answers2;
+        answers2 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("2. Умеете ли вы настоять на принятом решении или вас можно легко переубедить?"
+                ,2,answers2,1));
+        String[] answers3;
+        answers3 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("3. Любите ли вы брать на себя ответственность, руководить?"
+                ,2,answers3,1));
+        String[] answers4;
+        answers4 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("4. Пользуетесь ли вы уважением и доверием своих коллег?"
+                ,2,answers4,1));
+        String[] answers5;
+        answers5 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("5. Вы здоровы?"
+                ,2,answers5,1));
+        String[] answers6;
+        answers6 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("6. Готовы ли вы трудиться от зари до зари, не получая немедленной отдачи?"
+                ,2,answers6,1));
+        String[] answers7;
+        answers7 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("7. Любите ли вы общаться и работать с людьми?"
+                ,2,answers7,1));
+        String[] answers8;
+        answers8 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("8. Умеете ли вы убеждать других в правильности избранного пути?"
+                ,2,answers8,1));
+        String[] answers9;
+        answers9 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("9. Понятны ли вам идеи и мысли других?"
+                ,2,answers9,1));
+        String[] answers10;
+        answers10 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("10. Есть ли у вас опыт работы в той области, в которой вы хотите начать собственное дело?"
+                ,2,answers10,1));
+        String[] answers11;
+        answers11 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("11. Знакомы ли вы с действующими правилами налогообложения, калькуляции заработной платы, ведения бухгалтерского учета?"
+                ,2,answers11,1));
+        String[] answers12;
+        answers12 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("12. Будет ли в вашем городе или области спрос на товар или услугу, которые вы собираетесь предложить?"
+                ,2,answers12,1));
+        String[] answers13;
+        answers13 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("13. Есть ли у вас начальная подготовка в области маркетинга и финансов?"
+                ,2,answers13,1));
+        String[] answers14;
+        answers14 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("14. Хорошо ли идут дела в вашем городе (области) у других предпринимателей вашего профиля?"
+                ,2,answers14,1));
+        String[] answers15;
+        answers15 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("15. Есть ли у вас на примете помещение, которое можно арендовать?"
+                ,2,answers15,1));
+        String[] answers16;
+        answers16 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("16. Располагаете ли вы достаточными финансовыми средствами, чтобы поддержать свое предприятие в течение первого года его существования?"
+                ,2,answers16,1));
+        String[] answers17;
+        answers17 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("17. Есть ли у вас возможность привлечь к финансированию создаваемого вами предприятия родных и знакомых?"
+                ,2,answers17,1));
+        String[] answers18;
+        answers18 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("18. Есть ли у вас на примете поставщики необходимых вам материалов?"
+                ,2,answers18,1));
+        String[] answers19;
+        answers19 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("19. Есть ли у вас на примете толковые специалисты, обладающие опытом и знаниями, которых вам не хватает?"
+                ,2,answers19,1));
+        String[] answers20;
+        answers20 = new String[]{ "Да", "Нет"};
+        questions.add(new TestQuestion("20. Уверены ли вы в том, что иметь собственное дело — это главная ваша мечта?"
+                ,2,answers20,1));
+
+        Test test  = new Test("Тест: Предпринимательские способности","Тест позволяет оценить уровень Ваших предпринимательских способностей ", questions){
+            @Override
+            public String getResult() {
+                if (this.getCounterOfRight()>17){
+                    return "Вы имеете все необходимые качества, чтобы стать предпринимателем. Ваши целеустремленность, энергия и вера в успех помогут реализовать любую стоящую идею — главное, чтобы идеи действительно того заслуживали.";
+                }
+                if (this.getCounterOfRight()>=13&&this.getCounterOfRight()<17){
+                    return "Ваши шансы на успех в качестве предпринимателя не столь очевидны. Однако вы можете стать удачливым дельцом, коммерсантом.";
+                }
+                if (this.getCounterOfRight()>=0&&this.getCounterOfRight()<=12){
+                    return "Из вас вряд ли выйдет хороший предприниматель. Постарайтесь набраться опыта, и прежде чем встать на путь свободного предпринимательства, еще и еще раз сопоставьте свои интересы, желания и возможности.";
+                }
+
+                return "EROR";
             }
         };
         return test;
